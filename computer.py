32e39e529f24f14f78285fb08f22bfaa406d819d
Maanus Leesment <maanus.leesment@ttu.ee>
2015-10-26 18:59:04 +0200

    - Second commit

--- computer.py
+++ computer.py
@@ -1,29 +1,33 @@
 import urllib2
+import socket
 import sys
 
 def main():
-    myMachine = Server(10000)
-    myMachine.addComputersFromMachinesTxt()
-    myMachine.boot()
-    myMachine.listen()
-    
+    #TODO how to undbind the sockets?
+    my_machine = Server(10007)
+    my_machine.addComputersFromMachinesTxt()
+    my_machine.boot()
+    my_machine.listen() #should decide what to do with incoming GET/POST requests
+
 class Server:
     def __init__(self, port):
         #initiate a localhost server
-        self.ip = '127.0.0.1'
+        self.ip_address = '127.0.0.1'
         self.port = port
         self.sock = None
         self.computers = []
 
     def addComputersFromMachinesTxt(self):
         try:
-            f = open('machines.txt', 'r')
+            # input_file = open('machines.txt', 'r')
             # machines = f.read()
-            machines = [["localhost", "10001"],["127.0.0.1", "10002"],["localhost", "10002"]]
+            machines = [["localhost", "10001"], ["127.0.0.1", "10002"], ["localhost", "10002"]]
             computers = [Computer(machine[0], machine[1]) for machine in machines]
             self.addComputers(computers)
+        finally:
+            pass
         return None
-            
+
 
     def addComputers(self, computers):
         """
@@ -33,13 +37,13 @@ class Server:
             None
         """
         for computer in computers:
-            if (computer not in self.computers):
+            if computer not in self.computers:
                 self.computers.append(computer)
         return None
-        
 
-    def boot(self, ip, port):
-        self.address = (ip, port)
+
+    def boot(self):
+        self.address = (self.ip_address, self.port)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind(self.address)
         return None
@@ -55,33 +59,41 @@ class Server:
                 # Receive the data in small chunks and retransmit it
                 data_string = ""
                 while True:
-                    data = connection.recv(22)
-                    data_string += data
-                    if not data:
+                    data = connection.recv(32)
+                    print data
+                    if data:
+                        data_string += data
+                        connection.sendall(data)
+                    else:
+                        print "No more data"
                         break
-                print >>sys.stderr, 'the whole data', data_string
+
+                # print >>sys.stderr, 'the whole data', data_string
                 parse_result = self.parseDataString(data_string)
-                if (parse_result == "resourceRequest"):
+                print "I got behind the parsing thingy!"
+                if parse_result == "resourceRequest":
                     #TODO what to do in case of receiving a request about resource availability
                     pass
                 elif (parse_result == "jobTask"):
                     #TODO what to do in case we receive a task to solve
                     pass
-                else:
-                    
+            except:
+                print >>sys.stderr, "An error has occured while listening."
+
             finally:
                 # Clean up the connection
+                print "I closed the connection, yeeah."
                 connection.close()
-            
+
         return None
 
-    def parseDataString(data_string):
+    def parseDataString(self, data_string):
         #TODO try to parse it as a http header and determine what params
-        
+        return "Parsed Data String (http header)"
 
     def close(self):
         pass
-        
+
 
 class Computer:
     def __init__(self, ip, port):
@@ -91,35 +103,34 @@ class Computer:
         ip -- computers ip
         port -- computers port
         """
-        self.ip = ip
+        self.ip_address = ip
         self.port = port
         # Not important for slaves, Master deals with it
         self.resource_response = None
         self.task_to_solve = None
-        self.work_response = None 
-        
+        self.work_response = None
+
     def __str__(self):
-        return "my ip is: %s, port: %s" %(self.ip, self.port)
+        return "my ip is: %s, port: %s" %(self.ip_address, self.port)
 
     def prepare_resource_request_url(self):
-        base_url = "http://%s:%s/resource" %(self.ip, self.port)
+        base_url = "http://%s:%s/resource" %(self.ip_address, self.port)
         params = {"sendip":"",
                   "sendport":"",
                   "ttl":"",
                   "noask":""}
-        
+
         req = urllib2.Request(base_url)
         return req
 
     def send_resource_request(self):
-        
-        response = urllib2.urlopen()
+        url = None #TODO construct url
+        response = urllib2.urlopen(url)
         print response.info()
         html = response.read()
         # do something
         response.close()  # best practice to close the file
 
 
-if(__name__=="__main__"):
+if(__name__== "__main__"):
     main()
-    
--- tcp_client.py
+++ tcp_client.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10004)
+server_address = ('localhost', 10007)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
